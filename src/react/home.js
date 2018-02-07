7c1763ecaa99616e116d5438f4c7f61af3159b48
andrey <ovcharenkoav@meta.ua>
2018-02-07 20:52:15 +0200

    prop types

--- src/react/home.js
+++ src/react/home.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux'
 import {userActions} from '../redux/actions';
 
 
-class Home extends React.Component {
+class Home extends React.PureComponent {
   static async getInitialProps({ req, match, store, dispatch }) {
    console.log('getInitialProps');
    const userAgent = req ? req.headers['user-agent'] : navigator.userAgent
@@ -36,7 +37,7 @@ class Home extends React.Component {
       await Home.getInitialProps(this.props);
     }
     componentWillReceiveProps(){console.log('componentWillReceiveProps')}
-    shouldComponentUpdate(){
+    shouldComponentUpdate0(){
       console.log('shouldComponentUpdate')
       return true;
     }
@@ -51,8 +52,14 @@ class Home extends React.Component {
     //    props
     //    state
 
-
-
 }
+
+Home.propTypes = {
+  user: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    count: PropTypes.number.isRequired,
+    userAgent: PropTypes.string.isRequired,
+  }),
+};
 
 export default connect((state) => ({user: state.user})/*, mapDispatchToProps*/)(Home)
