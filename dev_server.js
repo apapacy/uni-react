e3c95038118542d610cd2c83e4f13f544b3833ed
Your Name <you@example.com>
2018-02-08 23:11:39 +0200

    server webpack clean

--- dev_server.js
+++ dev_server.js
@@ -2,56 +2,49 @@
 const path = require('path');
 const webpack = require('webpack');
 const compiler = webpack(require('./webpack.config.server'));
-const server = path.resolve(__dirname, './src/server.bundle.js');
+const serverPath = path.resolve(__dirname, './src/server.bundle.js');
 
-/*compiler.plugin("done", function(stats) {
-  if (started) {
-    return;
-  }
-  if (stats.compilation.errors && stats.compilation.errors.length) {
-    console.log('Server compile errors:', stats.compilation.errors);
-    require(server);
-    process.exit(5);
-  } else {
-    started = true;
-    watchServerChanges();
-  }
-});*/
+watch();
 
-
-
-let httpServer;
-const compilerOptions = {
-  aggregateTimeout: 300,
-  poll: 150,
-  ignored: server,
-};
-
-compiler.watch(compilerOptions, onServerChange);
-
-function onServerChange(err, stats) {
-  if (err) {
-    console.log('Server bundling error:', err);
-    return true;
-  }
-  if (httpServer && httpServer.httpServer) {
-    httpServer.httpServer.close(function() {
-      httpServer = initHttpServer();
-      console.log('Server restarted ' + new Date());
-    });
-    for (var socket of httpServer.sockets.values()) {
-      socket.destroy();
+function watch() {
+  let server;
+  const compilerOptions = {
+    aggregateTimeout: 300,
+    poll: 150,
+    ignored: server,
+  };
+  compiler.watch(compilerOptions, onServerChange);
+  function onServerChange(err, stats) {
+    if (err || stats.compilation.errors && stats.compilation.errors.length) {
+      console.log('Server bundling error:', err || stats.compilation.errors);
+      clearCache();
+      try {
+        require(serverPath);
+      } catch (ex) {
+        console.log('Error detecded', ex)
+      }
+      return;
     }
-  } else {
-    httpServer = initHttpServer();
-    console.log('Server bundling done');
+    if (server) {
+      for (const socket of server.sockets.values()) {
+        socket.destroy();
+      }
+      server.httpServer.close(function() {
+        server = initHttpServer();
+        console.log('Server restarted ' + new Date());
+      });
+    } else {
+      server = initHttpServer();
+      console.log('Server bundling done '  + new Date());
+    }
   }
 }
 
 function initHttpServer() {
+  let httpServer;
   try {
     clearCache();
-    httpServer = require(server);
+    httpServer = require(serverPath);
   } catch (ex) {
     console.log('Module require error: ', ex)
     return;
@@ -71,7 +64,7 @@ function initHttpServer() {
 function clearCache() {
   const cacheIds = Object.keys(require.cache);
   for (let id of cacheIds) {
-    if (id === server) {
+    if (id === serverPath) {
       delete require.cache[id];
       return;
     }
--- src/react/home.js
+++ src/react/home.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux'
-import {userActions} from '../redux/actions';
+import {userActions} from '../redux/action5s';
 
 class Home extends React.PureComponent {
   static async getInitialProps({ req, match, store, dispatch }) {
