ae610e73f316be0c0df7be5e01cdf732b85a29cb
Your Name <you@example.com>
2018-02-23 04:49:37 +0200

    linl href

--- src/react/pages/article.js
+++ src/react/pages/article.js
@@ -3,7 +3,7 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { me } from '../../redux/services/user';
 import { article, clearErrors } from '../../redux/services/article';
-
+import Link from '../asyncLink';
 
 class Article extends React.PureComponent {
   static async getInitialProps({ req, dispatch, user, match }) {
@@ -28,72 +28,59 @@ class Article extends React.PureComponent {
     <div className="container">
       <h1>{this.props.article.title}</h1>
       <div className="article-meta">
-        <a href=""><img src={this.props.article.author.image} /></a>
+        <Link to={`/author/${this.props.article.author.username}`}><img src={this.props.article.author.image} /></Link>
         <div className="info">
-          <a href="" className="author">{this.props.article.author.username}</a>
+          <Link to={`/author/${this.props.article.author.username}`} className="author">{this.props.article.author.username}</Link>
           <span className="date">{this.props.article.updatedAt}</span>
         </div>
         <button className="btn btn-sm btn-outline-secondary">
-          <i className="ion-plus-round"></i>
+          <i className="ion-plus-round" />
           &nbsp;
-          Follow Eric Simons <span className="counter">(10)</span>
+          Follow {this.props.article.author.username} <span className="counter" />
         </button>
         &nbsp;&nbsp;
-        {
-          this.props.article.favoritesCount
-            ?
-              <button className={`btn btn-sm btn-${this.props.article.favorited ? '' : 'outline-'}primary`}>
-                <i className="ion-heart" />
-                &nbsp;
-                Favorite Post <span className="counter">({this.props.article.favoritesCount})</span>
-              </button>
-            : null
-        }
+        <button className={`btn btn-sm btn-${this.props.article.favorited ? '' : 'outline-'}primary`}>
+          <i className="ion-heart" />
+          &nbsp;
+          Favorite Post <span className="counter">({this.props.article.favoritesCount})</span>
+        </button>
       </div>
 
     </div>
   </div>
-
   <div className="container page">
-
     <div className="row article-content">
       <div className="col-md-12">
-        <p>
-        Web development technologies have evolved at an incredible clip over the past few years.
-        </p>
-        <h2 id="introducing-ionic">Introducing RealWorld.</h2>
-        <p>It,s a great solution for learning how other frameworks work.</p>
+        {this.props.article.body}
       </div>
     </div>
-
     <hr />
-
     <div className="article-actions">
       <div className="article-meta">
-        <a href="profile.html"><img src="http://i.imgur.com/Qr71crq.jpg" /></a>
+        <Link to={`/author/${this.props.article.author.username}`}>
+          <img src={this.props.article.author.image} />
+        </Link>
         <div className="info">
-          <a href="" className="author">Eric Simons</a>
-          <span className="date">January 20th</span>
+          <Link to={`/author/${this.props.article.author.username}`} className="author">
+            {this.props.article.author.username}
+          </Link>
+          <span className="date">{this.props.article.updatedAt}</span>
         </div>
-
         <button className="btn btn-sm btn-outline-secondary">
-          <i className="ion-plus-round"></i>
+          <i className="ion-plus-round" />
           &nbsp;
-          Follow Eric Simons <span className="counter">(10)</span>
+          Follow {this.props.article.author.username}<span className="counter" />
         </button>
         &nbsp;
-        <button className="btn btn-sm btn-outline-primary">
-          <i className="ion-heart"></i>
+        <button className={`btn btn-sm btn-${this.props.article.favorited ? '' : 'outline-'}primary`}>
+          <i className="ion-heart" />
           &nbsp;
-          Favorite Post <span className="counter">(29)</span>
+          Favorite Post <span className="counter">{this.props.article.favoritesCount}</span>
         </button>
       </div>
     </div>
-
     <div className="row">
-
       <div className="col-xs-12 col-md-8 offset-md-2">
-
         <form className="card comment-form">
           <div className="card-block">
             <textarea className="form-control" placeholder="Write a comment..." rows="3"></textarea>
@@ -105,7 +92,6 @@ class Article extends React.PureComponent {
             </button>
           </div>
         </form>
-
         <div className="card">
           <div className="card-block">
             <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
@@ -119,7 +105,6 @@ class Article extends React.PureComponent {
             <span className="date-posted">Dec 29th</span>
           </div>
         </div>
-
         <div className="card">
           <div className="card-block">
             <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
@@ -132,18 +117,14 @@ class Article extends React.PureComponent {
             <a href="" className="comment-author">Jacob Schmidt</a>
             <span className="date-posted">Dec 29th</span>
             <span className="mod-options">
-              <i className="ion-edit"></i>
-              <i className="ion-trash-a"></i>
+              <i className="ion-edit" />
+              <i className="ion-trash-a" />
             </span>
           </div>
         </div>
-
       </div>
-
     </div>
-
   </div>
-
 </div>
 );
 }
